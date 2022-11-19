4bde0e6137844aa1f8ba30331a497ab3bc29e80d
Kyle <kylesalisbury2@gmail.com>
2022-11-19 16:03:40 +0000

    Add post request

--- website/auth.py
+++ website/auth.py
@@ -1,8 +1,8 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
     return render_template("login.html", text="Testing", boolean=True)
 
@@ -10,6 +10,23 @@ def login():
 def logout():
     return "<p>logout</p>"
 
-@auth.route('/sign-up')
+@auth.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        firstName = request.form.get('firstName')
+        password = request.form.get('password')
+        confirmPassword = request.form.get('confirmPassword')
+
+        if len(email) < 4:
+            flash('Email must be greater than 4 characters', category='error')
+        elif len(firstName) < 2:
+            flash('First name must be greater than 2 characters', category='error')
+        elif password != confirmPassword:
+            flash('Paswords don\'t match', category='error')
+        else:
+            #Add user
+            flash('Account created', category='success')
+            pass
+
     return render_template("signup.html")
