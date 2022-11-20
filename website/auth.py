548f3af63b5a602cfcaa20908551bc8d7b09e747
Kyle <kylesalisbury2@gmail.com>
2022-11-20 15:53:00 +0000

    Sign in functionality

--- website/auth.py
+++ website/auth.py
@@ -1,4 +1,7 @@
-from flask import Blueprint, render_template, request, flash
+from flask import Blueprint, render_template, request, flash, redirect, url_for
+from .models import User
+from werkzeug.security import generate_password_hash, check_password_hash
+from . import db
 
 auth = Blueprint('auth', __name__)
 
@@ -26,7 +29,12 @@ def sign_up():
             flash('Paswords don\'t match', category='error')
         else:
             #Add user
+            new_user = User(email=email, first_name=firstName, password=generate_password_hash(password, method='sha256'))
+            db.session.add(new_user)
+            db.session.commit()
+
             flash('Account created', category='success')
-            pass
+            
+            return redirect(url_for("views.home"))
 
     return render_template("signup.html")
--- website/models.py
+++ website/models.py
@@ -9,7 +9,7 @@ class Note(db.Model):
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
 class User(db.Model, UserMixin):
-    id = db.Columm(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(150), unique=True)
     password = db.Column(db.String(150))
     first_name = db.Column(db.String(150))
